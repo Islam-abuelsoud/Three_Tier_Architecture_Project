b96941593acbf099cadd93d88b8d8f09bc813a09
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2023-09-19 02:02:08 +0300

    empoleey Create

--- DesignPattern/Demo.DAL/Entites/Employee.cs
+++ DesignPattern/Demo.DAL/Entites/Employee.cs
@@ -12,11 +12,11 @@ namespace Demo.DAL.Entites
         public int Id { get; set; }
         [Required(ErrorMessage ="THis Feaild Is Requierd")]
         [MaxLength (50, ErrorMessage ="Max Length Is 50")]
-        [MinLength (50, ErrorMessage ="Min Length Is 5")]
+        [MinLength (5, ErrorMessage ="Min Length Is 5")]
         public string Name { get; set; }
         [Range(20,60,ErrorMessage ="range 20:60")]
         public int? Age { get; set; }
-        [RegularExpression(@"^(\\d{1,}) [a-zA-Z0-9\\s]+(\\,)? [a-zA-Z]+(\\,)? [A-Z]{2} [0-9]{5,6}$",ErrorMessage = "Address Must be Like -3344 W Alameda Avenue, Lakewood, CO 80222-")]
+        //[RegularExpression(@"^(\\d{1,}) [a-zA-Z0-9\\s]+(\\,)? [a-zA-Z]+(\\,)? [A-Z]{2} [0-9]{5,6}$",ErrorMessage = "Address Must be Like -3344 W Alameda Avenue, Lakewood, CO 80222-")]
         public string Address { get; set; }
         [DataType(DataType.Currency)]
         [Range(4000,8000,ErrorMessage ="selary must be btween 4K : 8K")]
--- DesignPattern/Demo.PL/Controllers/EmployeeController.cs
+++ DesignPattern/Demo.PL/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
 ﻿using Demo.BLL.Interfaces;
+using Demo.BLL.repositories;
+using Demo.DAL.Entites;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.PL.Controllers
@@ -10,6 +12,84 @@ namespace Demo.PL.Controllers
         {
             EmployeeRepo = employeeRepo;
         }
+        public IActionResult Index()
+        {
+            return View(EmployeeRepo.GetAll());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeRepo.Add(Employee);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(Employee);
+            }
+        }
+        public IActionResult Details(int? id, string ViewName = "Details")
+        {
+            if (id == null)
+                return NotFound();
+            var dep = EmployeeRepo.get(id);
+            if (dep == null)
+                return NotFound();
+            return View(ViewName, dep);
+        }
+        public IActionResult Edit(int? Id)
+        {
+            return Details(Id, "Edit");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int Id, Employee Employee)
+        {
+            if (Id != Employee.Id)
+                return BadRequest();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    EmployeeRepo.Update(Employee);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        public IActionResult Delete(int? Id)
+        {
+            return Details(Id, "Delete");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int? Id, Employee Employee)
+        {
+            if (Id != Employee.Id)
+            {
+                return NotFound();
+            }
+            try
+            {
+                EmployeeRepo.Delete(Employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (System.Exception)
+            {
 
+                return BadRequest();
+            }
+
+        }
     }
 }
